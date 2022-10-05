89391aeec74c86521d4aff3fbd51715ab69a61b8
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 00:04:57 +0900

    Setup prisma

---
+++ backend/prisma/migrations/20221005150444_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "TodoStatus" AS ENUM ('done', 'pending');
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "status" "TodoStatus" NOT NULL DEFAULT 'pending',
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
