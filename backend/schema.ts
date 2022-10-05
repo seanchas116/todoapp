ec052bdb2007278d7a5cbe49aa988950b580bb51
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 00:32:10 +0900

    WIP schema

--- backend/schema.ts
+++ backend/schema.ts
@@ -1,14 +1,26 @@
 import SchemaBuilder from "@pothos/core";
+import { User } from "@prisma/client";
 
-const builder = new SchemaBuilder({});
+const builder = new SchemaBuilder<{
+  Objects: {
+    User: User;
+  };
+}>({});
+
+builder.objectType("User", {
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    name: t.exposeString("name", {
+      nullable: true,
+    }),
+  }),
+});
 
 builder.queryType({
   fields: (t) => ({
-    hello: t.string({
-      args: {
-        name: t.arg.string(),
-      },
-      resolve: (parent, { name }) => `Hello ${name || "World"}!`,
+    users: t.field({
+      type: ["User"],
+      resolve: async () => [],
     }),
   }),
 });
