fd298fba03281b2ffb2f0f798e259d488f714a54
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 14:52:58 +0900

    Add todos query

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,10 +1,11 @@
 import SchemaBuilder from "@pothos/core";
-import { User } from "@prisma/client";
+import { Todo, User } from "@prisma/client";
 import { prisma } from "./prisma";
 
 const builder = new SchemaBuilder<{
   Objects: {
     User: User;
+    Todo: Todo;
   };
 }>({});
 
@@ -22,6 +23,13 @@ builder.queryType({
     users: t.field({
       type: ["User"],
       resolve: async () => prisma.user.findMany(),
+    }),
+    todos: t.field({
+      type: ["Todo"],
+      resolve: async () => {
+        // TODO: filter by current user
+        return prisma.todo.findMany();
+      },
     }),
   }),
 });
