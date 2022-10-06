c078ba8d3a3eb09f9a275213135e505ddd2947a3
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 14:22:54 +0900

    Sign in/out

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
+  const { loginWithPopup, isAuthenticated, logout } = useAuth0();
+
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello World</h1>
+    <div className="m-4">
+      <h1 className="pb-4 text-xl font-bold">Todo App</h1>
+      {isAuthenticated ? (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => logout()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => loginWithPopup()}
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="seanchas116.auth0.com"
+      clientId="zccoMdGHuJIdz98w3CV8TCCtHFGU323m"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
-)
+);
