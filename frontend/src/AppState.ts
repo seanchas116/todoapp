d292abf1b667acb7f981ed7be724e250a70dbdbd
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 16:28:24 +0900

    Send token

--- frontend/src/AppState.ts
+++ frontend/src/AppState.ts
@@ -1,4 +1,10 @@
-import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import createAuth0Client from "@auth0/auth0-spa-js";
 import { makeObservable, observable } from "mobx";
 
@@ -8,8 +14,22 @@ const auth0 = await createAuth0Client({
   redirect_uri: window.location.origin,
 });
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await auth0.getTokenSilently();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -17,34 +37,39 @@ export class AppState {
   constructor() {
     void this.init();
     makeObservable(this);
-
-    client
-      .query({
-        query: gql`
-          query {
-            todos {
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => console.log(result));
   }
 
+  accessToken: string | undefined;
   @observable isAuthenticated = false;
 
   async init() {
-    await auth0.getTokenSilently();
+    this.accessToken = await auth0.getTokenSilently();
     this.isAuthenticated = true;
+    await this.fetchTodos();
   }
 
   async login() {
     await auth0.loginWithPopup();
     this.isAuthenticated = true;
+    await this.fetchTodos();
   }
 
   async logout() {
     await auth0.logout();
+  }
+
+  async fetchTodos() {
+    const todos = await client.query({
+      query: gql`
+        query {
+          todos {
+            title
+          }
+        }
+      `,
+    });
+
+    console.log(todos);
   }
 }
 
