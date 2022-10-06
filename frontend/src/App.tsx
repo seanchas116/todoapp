fe069f9b1ae2d05fc00357c308c0346a9115cea0
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-06 18:42:02 +0900

    Tweak style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,25 @@ import { appState } from "./AppState";
 
 const App = observer(function App() {
   return (
-    <div className="m-4">
-      <h1 className="pb-4 text-xl font-bold">Todo App</h1>
-      {appState.isAuthenticated ? (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => appState.logout()}
-        >
-          Sign Out
-        </button>
-      ) : (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => appState.login()}
-        >
-          Sign In
-        </button>
-      )}
+    <div className="">
+      <header className="p-4 flex items-center justify-between border-b border-gray-200">
+        <h1 className="text-xl font-bold">Todo App</h1>
+        {appState.isAuthenticated ? (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded"
+            onClick={() => appState.logout()}
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded"
+            onClick={() => appState.login()}
+          >
+            Sign In
+          </button>
+        )}
+      </header>
     </div>
   );
 });
