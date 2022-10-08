b95ae806a03e14d9921b99f18a356efb179d05b9
Ryohei Ikegami <iofg2100@gmail.com>
2022-10-08 20:36:48 +0900

    Connect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,28 @@ import { createServer } from "@graphql-yoga/node";
 import { Context } from "./context";
 import { getUserFromAuthHeader } from "./firebase";
 import { schema } from "./schema";
+import { Server as SocketIOServer } from "socket.io";
 
-const server = createServer({
-  schema,
-  context: async ({ req }): Promise<Context> => ({
-    currentUser: await getUserFromAuthHeader(req.headers.authorization),
-  }),
-  maskedErrors: false,
-});
+async function init() {
+  const server = createServer({
+    schema,
+    context: async ({ req }): Promise<Context> => ({
+      currentUser: await getUserFromAuthHeader(req.headers.authorization),
+    }),
+    maskedErrors: false,
+  });
 
-server.start();
+  const httpServer = await server.start();
+
+  const io = new SocketIOServer(httpServer, {
+    cors: {
+      origin: "http://localhost:5173",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("connect socket", socket.id);
+  });
+}
+
+init();
--- frontend/src/state/AppState.ts
+++ frontend/src/state/AppState.ts
@@ -1,11 +1,15 @@
 import { CurrentUserStore } from "./CurrentUserStore";
 import { TodoStore } from "./TodoStore";
+import { io, Socket } from "socket.io-client";
 
 export class AppState {
-  constructor() {}
+  constructor() {
+    this.socket = io("http://localhost:4000");
+  }
 
   readonly currentUserStore = new CurrentUserStore();
   readonly todoStore = new TodoStore();
+  readonly socket: Socket;
 }
 
 export const appState = new AppState();
